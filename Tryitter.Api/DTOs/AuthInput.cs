a00bde374e862c26b4d20404224944935d344dc5
Juliano Boese <juliano.boese@gmail.com>
2022-12-10 19:07:11 -0300

    feat: create DTOs

---
+++ Tryitter.Api/DTOs/AuthInput.cs
@@ -0,0 +1,8 @@
+﻿namespace Tryitter.Api.DTOs
+{
+    public class AuthInput
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Tryitter.Api/DTOs/AuthOutput.cs
@@ -0,0 +1,10 @@
+﻿using Tryitter.Api.Models;
+
+namespace Tryitter.Api.ViewModels
+{
+    public class AuthOutput
+    {
+        public Student Student { get; set; }
+        public string Token { get; set; }
+    }
+}
