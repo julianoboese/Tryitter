b273d8de50bbbcbf97c54f65421f199c48cb5b9c
Juliano Boese <juliano.boese@gmail.com>
2022-12-14 13:08:26 -0300

    chore: remove padlock from public endpoints in swagger

---
+++ Tryitter.Api/AuthOperationFilter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Tryitter.Api
+{
+    public class AuthOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var noAuthRequired = context.ApiDescription.CustomAttributes().Any(attr => attr.GetType() == typeof(AllowAnonymousAttribute));
+
+            if (noAuthRequired) return;
+
+            operation.Security = new List<OpenApiSecurityRequirement>
+        {
+            new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "basic"
+                        }
+                    },
+                    new List<string>()
+                }
+            }
+        };
+        }
+    }
+}
